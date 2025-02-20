360107026272d9c44ceb3b32361c63c08ef64a5e
Darlley <systembrtt@gmail.com>
2025-02-19 22:03:42 -0400

    ORM or DB Client ✅

---
+++ prisma/migrations/20250220015944_first/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "_id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT,
+    "isCompleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
